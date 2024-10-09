2fe9cff517016b3c108226a0e82832ce2c9da22b
NicolasJ88 <168785550+NicolasJ88@users.noreply.github.com>
2024-10-09 19:27:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,461 @@
+let productos = [
+   //conjuntos inicio//
+    {
+        "id": "abrigo-01",
+        "titulo": "Conjunto Deportivo Femenino",
+        "imagen": "https://i.blogs.es/1354de/prendas-de-ropa-deportiva-3-/1366_2000.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 99000
+    },
+    {
+        "id": "abrigo-02",
+        "titulo": "Conjunto Deportivo River Plate",
+        "imagen": "https://www.opensports.com.ar/media/catalog/product/cache/4769e4d9f3516e60f2b4303f8e5014a8/F/Q/FQ7674_0.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 129999
+    },
+    {
+        "id": "abrigo-03",
+        "titulo": "Conjunto Deportivo Puma",
+        "imagen": "https://images.puma.net/images/848108/01/mod01/fnd/MEX/",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 120000
+    },
+    {
+        "id": "abrigo-04",
+        "titulo": "Conjunto Deportivo Nike Femenino",
+        "imagen": "https://buzosdeportivos.com/wp-content/uploads/2020/01/buzos-deportivos-nike-mujer-windrunner-lila-768x768.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 89999
+    },
+    {
+        "id": "abrigo-05",
+        "titulo": "Conjunto Deportivo Nike Hombre",
+        "imagen": "https://falabella.scene7.com/is/image/FalabellaCO/43859444_1?wid=800&hei=800&qlt=70",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 200000
+    },
+    {
+        "id": "abrigo-06",
+        "titulo": "Conjunto Deportivo Adidas Essentials",
+        "imagen": "https://www.opensports.com.ar/media/catalog/product/cache/4769e4d9f3516e60f2b4303f8e5014a8/G/K/GK9658_0_6.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 119999
+    },
+    {
+        "id": "abrigo-07",
+        "titulo": "Conjunto Adidas Adicolor",
+        "imagen": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/634c84e86e0b4eae83eeae75010928ba_9366/Campera_Deportiva_Adicolor_Classics_Primeblue_SST_Azul_HK7340_21_model.jpg",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 150000
+    },
+    {
+        "id": "abrigo-08",
+        "titulo": "Conjunto Deportivo Ropa Mujer",
+        "imagen": "https://falabella.scene7.com/is/image/FalabellaPE/19629322_1?wid=800&hei=800&qlt=70",
+        "categoria": {
+            "nombre": "Abrigos",
+            "id": "abrigos"
+        },
+        "precio": 139999
+    },
+
+//conjuntos fin//
+
+//camisetas inicio//
+    {
+        "id": "camiseta-01",
+        "titulo": "Camiseta River Plate 2023/4",
+        "imagen": "https://img.planetafobal.com/2023/09/river-alternativa-2023-2024-adidas-camiseta-zaq.webp",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 120000
+    },
+    {
+        "id": "camiseta-02",
+        "titulo": "Camiseta AC Milan 2021",
+        "imagen": "https://todosobrecamisetas.com/wp-content/uploads/ac-milan-2022-23-puma-home-kit-1.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 98999
+    },
+    {
+        "id": "camiseta-03",
+        "titulo": "Camiseta Nike Blanca",
+        "imagen": "https://imgnike-a.akamaihd.net/1920x1920/01638853.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 45000
+    },
+    {
+        "id": "camiseta-04",
+        "titulo": "Camiseta FC Barcelona 2022/23",
+        "imagen": "https://www.futbolemotion.com/imagesarticulos/169128/grandes/camiseta-nike-fc-barcelona-primera-equipacion-stadium-2022-2023-mujer-obsidian-game-royal-0.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 99999
+    },
+    {
+        "id": "camiseta-05",
+        "titulo": "Camiseta Puma Negra",
+        "imagen": "https://imgmarketplace.lojasrenner.com.br/20001/2836/701872440/755496351/0.jpeg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 85000
+    },
+    {
+        "id": "camiseta-06",
+        "titulo": "Camiseta Alternativa River Plate 2016",
+        "imagen": "https://www.digitalsport.com.ar/files/products/56fbe5829135a-372291-1200x1200.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 110000
+    },
+    {
+        "id": "camiseta-07",
+        "titulo": "Camiseta AC Milan 2006/7",
+        "imagen": "https://d2j6dbq0eux0bg.cloudfront.net/images/15913120/1817743672.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 119000
+    },
+    {
+        "id": "camiseta-08",
+        "titulo": "Camiseta Real Madrid Femenina",
+        "imagen": "https://images.prodirectsport.com/ProductImages/Main/255239_Main_Thumb_1181902.jpg",
+        "categoria": {
+            "nombre": "Camisetas",
+            "id": "camisetas"
+        },
+        "precio": 99000
+    },
+//camisetas fin//
+
+//pantalones inicio//
+    {
+        "id": "pantalon-01",
+        "titulo": "Pantalón Casual Puma Femenino",
+        "imagen": "https://www.innovasport.com/medias/IS-673978-01-4.jpg?context=bWFzdGVyfGltYWdlc3wxNjIzOTV8aW1hZ2UvanBlZ3xpbWFnZXMvaDUzL2gwNC8xMTUyMjYxMDIzMzM3NC5qcGd8NGFiZTcwNjI1YzhlNjNkMjYxNjM2MmQwMTk0Njk4MTRhYmQwMjZlMWQ4ZDEzMzkyY2VlODI0ZTFjZDEyYWM0Yg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 99000
+    },
+    {
+        "id": "pantalon-02",
+        "titulo": "Pantalón River Plate",
+        "imagen": "https://www.digitalsport.com.ar/files/products/6101bd335e5ad-525602-500x500.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 110000
+    },
+    {
+        "id": "pantalon-03",
+        "titulo": "Pantalón Deportivo Femenino Puma Essential",
+        "imagen": "https://www.tennis-point.es/dw/image/v2/BBDP_PRD/on/demandware.static/-/Sites-master-catalog/default/dwda2195e9/images/079/446/01128000__20.jpg?q=80&sw=2000",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 119000
+    },
+    {
+        "id": "pantalon-04",
+        "titulo": "Pantalón Adidas Tricolor",
+        "imagen": "https://assets.adidas.com/images/w_1880,f_auto,q_auto/2b07d53383ac4d2fb186af0100c5b476_9366/IB3430_HM5.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 250000
+    },
+    {
+        "id": "pantalon-05",
+        "titulo": "Pantalón Adidas Men Tiro 21",
+        "imagen": "https://www.rebelsport.com.au/dw/image/v2/BBRV_PRD/on/demandware.static/-/Sites-srg-internal-master-catalog/default/dw2ea89f07/images/60479101/Rebel_60479101-01_black_hi-res.jpg?sw=1000&sh=1000&sm=fit",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 120000
+    },
+    {
+        "id": "pantalon-06",
+        "titulo": "Pantalón Nike Mujer",
+        "imagen": "https://megasports.vteximg.com.br/arquivos/ids/199319-1000-1000/06190041001_2.jpg?v=637625561894800000",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 115000
+    },
+    {
+        "id": "pantalon-07",
+        "titulo": "Pantalón Joggin Hombre",
+        "imagen": "https://assets.laboutiqueofficielle.com/w_1100,q_auto,f_auto/media/products/2018/10/06/adidas_157917_DH5825_20181012T141834_02.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 170000
+    },
+    {
+        "id": "pantalon-08",
+        "titulo": "Pantalón Deportivo España 2024",
+        "imagen": "https://www.futbolemotion.com/imagesarticulos/189811/750/pantalon-largo-adidas-espana-training-mundial-femenino-2023-mujer-mystery-ruby-3.jpg",
+        "categoria": {
+            "nombre": "Pantalones",
+            "id": "pantalones"
+        },
+        "precio": 190000
+    },
+
+    //pantalones fin//
+    //zapatillas inicio//
+    {
+        "id": "zapatilla-01",
+        "titulo": "Air Jordan Retro High",
+        "imagen": "https://nikearprod.vtexassets.com/arquivos/ids/1037185-1200-1200?width=1200&height=1200&aspect=true",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 230000
+    },
+    {
+        "id": "zapatilla-02",
+        "titulo": "Nike Zion Unisex",
+        "imagen": "https://nikearprod.vtexassets.com/arquivos/ids/870477-1200-1200?width=1200&height=1200&aspect=true",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 145000
+    },
+    {
+        "id": "zapatilla-03",
+        "titulo": "Air Jordan 1 Low",
+        "imagen": "https://nikearprod.vtexassets.com/arquivos/ids/869515-1200-1200?width=1200&height=1200&aspect=true",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 119000
+    },
+    {
+        "id": "zapatilla-04",
+        "titulo": "Converse Hombre",
+        "imagen": "https://www.lapolar.cl/dw/image/v2/BCPP_PRD/on/demandware.static/-/Sites-master-catalog/default/dw60dc99cd/images/large/2CC20024348.jpg?sw=1200&sh=1200&sm=fit",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 170000
+    },
+    {
+        "id": "zapatilla-05",
+        "titulo": "Nike Jordan Miles Morales",
+        "imagen": "https://media.endclothing.com/media/f_auto,w_600,h_600/prodmedia/media/catalog/product/1/2/12-12-2018_nikeairjordan_1retrohighog_gymredblackwhitephotoblue_555088-602_gh_1.jpg",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 230000
+    },
+    {
+        "id": "zapatilla-06",
+        "titulo": "Botines Nike Mercury",
+        "imagen": "https://www.prodirectsoccer.com/es/productimages/V3_1_Main/150122.jpg",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 190000
+    },
+    {
+        "id": "zapatilla-07",
+        "titulo": "Vans Old Skool Hombre",
+        "imagen": "https://vanscl.vtexassets.com/arquivos/ids/765431/VN000D3H_Y28_1.jpg?v=638017176607500000",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 165000
+    },
+    {
+        "id": "zapatilla-08",
+        "titulo": "Air Jordan Prowler Edition",
+        "imagen": "https://preview.redd.it/anyone-know-if-there-is-anywhere-to-cop-the-new-jordan-1-v0-tbllkviehi6b1.jpg?width=1080&crop=smart&auto=webp&s=6f98b7bcaf4cf2ff02a92f5fb7f24c70aa574d0b",
+        "categoria": {
+            "nombre": "Zapatillas",
+            "id": "Zapatillas"
+        },
+        "precio": 450000
+    },
+
+    //zapatillas fin//
+
+];
+
+fetch("productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
+
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
